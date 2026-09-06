f60de807637dbd354488469e69087133e80117e9
kanikarthika18-design <kanikarthika18@gmail.com>
2026-09-06 13:14:18 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -21,7 +21,7 @@ function createExam() {
 // =====================================
 
 function addQuestions() {
-    window.location.href = "questions.html";
+    window.location.href = "question.html";
 }
 
 
